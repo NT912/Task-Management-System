e3a2f5e4ca3013f14a4d16c08d23ff9739d8315e
NHATRUONG <tt912002@gmail.com>
2026-01-12 22:30:54 +0700

    [backend][feat]: add env

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,11 +1,13 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Tạo một "Pool" kết nối để tối ưu hiệu suất (tái sử dụng các kết nối thay vì tạo mới liên tục)
 export const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'truongdeptrai',
-  database: 'task_management',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
